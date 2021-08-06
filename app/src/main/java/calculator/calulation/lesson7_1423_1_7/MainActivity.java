50910465e3cce09af276234fb0ba3337db360836
Andej Nesterenko <mentatus.n@gmail.com>
2021-08-06 22:40:25 +0300

    Внедрили свой toolbar
    Реализовали:
    -Options menu
    -Popup menu

--- app/src/main/java/calculator/calulation/lesson7_1423_1_7/MainActivity.java
+++ app/src/main/java/calculator/calulation/lesson7_1423_1_7/MainActivity.java
@@ -1,13 +1,22 @@
 package calculator.calulation.lesson7_1423_1_7;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
+import java.util.List;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -16,15 +25,45 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+        readSettings();
+        initToolbar();
+    }
 
+    private void initToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_main:
+                showFragment(MainFragment.newInstance());
+                break;
+            case R.id.action_favorite:
+                showFragment(FavoriteFragment.newInstance());
+                break;
+            case R.id.action_settings:
+                showFragment(SettingsFragment.newInstance());
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void readSettings() {
         SharedPreferences sharedPreferences = getSharedPreferences(Settings.SHARED_FILE_NAME, Context.MODE_PRIVATE);
-
-        Settings.isDeleteFragment = sharedPreferences.getBoolean(Settings.IS_DELETE_FRAGMENT_BEFORE_ADD,false);
-        Settings.isBackIsRemove = sharedPreferences.getBoolean(Settings.IS_BACK_IS_REMOVE_FRAGMENT,false);
-        Settings.isBackStack = sharedPreferences.getBoolean(Settings.IS_BACK_STACK_USED,false);
-        Settings.isReplaceFragment = sharedPreferences.getBoolean(Settings.IS_REPLACE_FRAGMENT_USED,false);
-        Settings.isAddFragment = sharedPreferences.getBoolean(Settings.IS_ADD_FRAGMENT_USED,false);
-
+        Settings.isDeleteFragment = sharedPreferences.getBoolean(Settings.IS_DELETE_FRAGMENT_BEFORE_ADD, false);
+        Settings.isBackIsRemove = sharedPreferences.getBoolean(Settings.IS_BACK_IS_REMOVE_FRAGMENT, false);
+        Settings.isBackStack = sharedPreferences.getBoolean(Settings.IS_BACK_STACK_USED, false);
+        Settings.isReplaceFragment = sharedPreferences.getBoolean(Settings.IS_REPLACE_FRAGMENT_USED, false);
+        Settings.isAddFragment = sharedPreferences.getBoolean(Settings.IS_ADD_FRAGMENT_USED, false);
     }
 
     private void initView() {
@@ -43,6 +82,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.buttonBack:
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                if(fragmentManager.getFragments().size()<=1) break;
+                if( Settings.isBackIsRemove){
+                    Fragment fragmentForDelete = getVisibleFragment(fragmentManager);
+                    if(fragmentForDelete!=null){
+                        fragmentManager.beginTransaction().remove(fragmentForDelete).commit();
+                    }
+                }else {
+                    fragmentManager.popBackStack();
+                }
                 break;
             case R.id.buttonMain:
                 showFragment(MainFragment.newInstance());
@@ -57,11 +106,42 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    private void showFragment(Fragment fragment) {
-        
-        getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.fragment_container, fragment)
-                .commit();
+    Fragment getVisibleFragment(FragmentManager fragmentManager) {
+        List<Fragment> fList = fragmentManager.getFragments();
+        for (int i = 0; i < fList.size(); i++) {
+            Fragment fragment = fList.get(i);
+            if (fragment.isVisible()) {
+                return fragment;
+            }
+        }
+        return null;
+    }
+
+    public void showFragment(Fragment fragment) {
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        if (Settings.isDeleteFragment) {
+            Fragment fragmentForDelete = getVisibleFragment(fragmentManager);
+            if (fragmentForDelete != null) {
+                fragmentTransaction.remove(fragmentForDelete);
+            }
+        }
+
+        if (Settings.isAddFragment) {
+            fragmentTransaction
+                    .add(R.id.fragment_container, fragment);
+        } else if (Settings.isReplaceFragment) {
+            fragmentTransaction
+                    .replace(R.id.fragment_container, fragment);
+        }
+
+        if(Settings.isBackStack){
+            fragmentTransaction.addToBackStack("");
+        }
+
+        fragmentTransaction.commit();
+
     }
 }
--- app/src/main/java/calculator/calulation/lesson7_1423_1_7/MainFragment.java
+++ app/src/main/java/calculator/calulation/lesson7_1423_1_7/MainFragment.java
@@ -2,8 +2,11 @@ package calculator.calulation.lesson7_1423_1_7;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.PopupMenu;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -18,6 +21,32 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView( LayoutInflater inflater, ViewGroup container,  Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_main,container,false);
+        ( (TextView) v.findViewById(R.id.textView)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu popupMenu = new PopupMenu(requireActivity(), v);
+                requireActivity().getMenuInflater().inflate(R.menu.popup,popupMenu.getMenu());
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()){
+                            case R.id.action_main:
+                                ((MainActivity)requireActivity()).showFragment(MainFragment.newInstance());
+                                return true; // TODO 1
+                                //break;
+                            case R.id.action_favorite:
+                                ((MainActivity)requireActivity()).showFragment(FavoriteFragment.newInstance());
+                                break;
+                            case R.id.action_settings:
+                                ((MainActivity)requireActivity()).showFragment(SettingsFragment.newInstance());
+                                break;
+                        }
+                        return false;
+                    }
+                });
+                popupMenu.show();
+            }
+        });
         return v;
     }
 }
